15f1908a842846c14e9ab47403313bb08b595a83
misterbobr <monsieurraspberry@gmail.com>
2022-11-04 03:44:39 +0600

    added technologies descriptions to localization file

--- dist/js/cases.js
+++ dist/js/cases.js
@@ -1,1 +1,74 @@
-import{gsap}from"gsap";import{ScrollTrigger}from"gsap/ScrollTrigger";gsap.registerPlugin(ScrollTrigger);export function loadCases(){gsap.to(".border_width",{clipPath:"polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)",scrollTrigger:{trigger:".cases_trigger",start:"bottom 300",end:"top top",toggleActions:"play none reverse none"}}),gsap.to(".border_height",{clipPath:"polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)",delay:.3,scrollTrigger:{trigger:".cases_trigger",start:"bottom 300",end:"top top",toggleActions:"play none reverse none"}}),gsap.to("#root-index",{background:"rgb(135,129,123)",scrollTrigger:{trigger:".bg_color-trigger",start:"top 300",end:"top top",toggleActions:"play none reverse none",scrub:6}}),gsap.to(".hdr",{background:"rgb(135,129,123)",scrollTrigger:{trigger:".bg_color-trigger",start:"top 300",end:"top top",toggleActions:"play none reverse none",scrub:6}})}
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+export function loadCases() {
+    // Появления бордеров у проектов по триггеру
+    gsap.to(".border_width", {
+        clipPath: "polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)",
+        // ease: "expo.inOut",
+        // duration: 1.5,
+
+        scrollTrigger: {
+            trigger: ".cases_trigger",
+            start: "bottom 300",
+            end: "top top",
+            toggleActions: "play none reverse none",
+            // markers: true,
+            // id: "scrub"
+        }
+    });
+
+
+    gsap.to(".border_height", {
+        clipPath: "polygon(100% 100%, 0% 100%, 0% 0%, 100% 0%)",
+        delay: .3,
+        // ease: "expo.inOut",
+        // duration: 1.5,
+
+        scrollTrigger: {
+            trigger: ".cases_trigger",
+            start: "bottom 300",
+            end: "top top",
+            toggleActions: "play none reverse none",
+            // markers: true,
+            // id: "scrub"
+        }
+    });
+    // /Появления бордеров у проектов по триггеру
+
+    // Смена цвета страницы и цвета хедера
+    gsap.to("#root-index", {
+        background: 'rgb(135,129,123)',
+        // ease: "expo.inOut",
+        // duration: 1.5,
+
+        scrollTrigger: {
+            trigger: ".bg_color-trigger",
+            start: "top 300",
+            end: "top top",
+            toggleActions: "play none reverse none",
+            // markers: true,
+            scrub: 6,
+            // id: "scrub"
+        }
+    });
+
+    gsap.to(".hdr", {
+        background: 'rgb(135,129,123)',
+        // ease: "expo.inOut",
+        // duration: 1.5,
+
+        scrollTrigger: {
+            trigger: ".bg_color-trigger",
+            start: "top 300",
+            end: "top top",
+            toggleActions: "play none reverse none",
+            // markers: true,
+            scrub: 6,
+            // id: "scrub"
+        }
+    });
+    // /Смена цвета страницы и цвета хедера
+}
