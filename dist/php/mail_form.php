b0e99ed1754c40030c387106ab2280bc15560445
misterbobr <monsieurraspberry@gmail.com>
2023-01-24 17:14:56 +0300

    fixing message body

--- dist/php/mail_form.php
+++ dist/php/mail_form.php
@@ -64,77 +64,75 @@ $messageBody = "<html>
 
 //Create an instance; passing `true` enables exceptions
 
-class Mailer extends PHPMailer { 
-    public static function sendMail($email, $messageBody) {
-        $mail = new PHPMailer(true); 
-        $OAUTH_USER_EMAIL    = $_ENV['OAUTH_USER_EMAIL'];
-        $OAUTH_CLIENT_ID     = $_ENV['OAUTH_CLIENT_ID'];
-        $OAUTH_CLIENT_SECRET = $_ENV['OAUTH_CLIENT_SECRET'];
-        $OAUTH_REFRESH_TOKEN = $_ENV['OAUTH_REFRESH_TOKEN'];
-        // echo $OAUTH_USER_EMAIL    ."<br>";
-        // echo $OAUTH_CLIENT_ID     ."<br>";
-        // echo $OAUTH_CLIENT_SECRET ."<br>";
-        // echo $OAUTH_REFRESH_TOKEN ."<br>";
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->CharSet = PHPMailer::CHARSET_UTF8;
-            $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->AuthType = 'XOAUTH2';
-            // $mail->Username   = $GOOGLE_SMTP_USERNAME;                     //SMTP username
-            // $mail->Password   = $GOOGLE_SMTP_PASS;                               //SMTP password
-            // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            // $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+$mail = new PHPMailer(true); 
+$OAUTH_USER_EMAIL    = $_ENV['OAUTH_USER_EMAIL'];
+$OAUTH_CLIENT_ID     = $_ENV['OAUTH_CLIENT_ID'];
+$OAUTH_CLIENT_SECRET = $_ENV['OAUTH_CLIENT_SECRET'];
+$OAUTH_REFRESH_TOKEN = $_ENV['OAUTH_REFRESH_TOKEN'];
+// echo $OAUTH_USER_EMAIL    ."<br>";
+// echo $OAUTH_CLIENT_ID     ."<br>";
+// echo $OAUTH_CLIENT_SECRET ."<br>";
+// echo $OAUTH_REFRESH_TOKEN ."<br>";
+    
+//Server settings
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+$mail->isSMTP();                                            //Send using SMTP
+$mail->CharSet = PHPMailer::CHARSET_UTF8;
+$mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->AuthType = 'XOAUTH2';
+// $mail->Username   = $GOOGLE_SMTP_USERNAME;                     //SMTP username
+// $mail->Password   = $GOOGLE_SMTP_PASS;                               //SMTP password
+// $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+// $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+$mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-            // Setting OAuth authentication
-            $provider = new Google(
-                [
-                    'clientId' => $OAUTH_CLIENT_ID,
-                    'clientSecret' => $OAUTH_CLIENT_SECRET
-                ]
-            );
-            $mail->setOAuth(
-                new OAuth(
-                    [
-                        'provider' => $provider,
-                        'clientId' => $OAUTH_CLIENT_ID,
-                        'clientSecret' => $OAUTH_CLIENT_SECRET,
-                        'refreshToken' => $OAUTH_REFRESH_TOKEN,
-                        'userName' => $OAUTH_USER_EMAIL,
-                    ]
-                    )
-                );
+// Setting OAuth authentication
+$provider = new Google(
+    [
+        'clientId' => $OAUTH_CLIENT_ID,
+        'clientSecret' => $OAUTH_CLIENT_SECRET
+    ]
+);
+$mail->setOAuth(
+    new OAuth(
+        [
+            'provider' => $provider,
+            'clientId' => $OAUTH_CLIENT_ID,
+            'clientSecret' => $OAUTH_CLIENT_SECRET,
+            'refreshToken' => $OAUTH_REFRESH_TOKEN,
+            'userName' => $OAUTH_USER_EMAIL,
+        ]
+        )
+    );
 
-            //Recipients
-            $mail->Subject = "Новое сообщение из формы";
-            $mail->setFrom('mailbot@elegy.studio');
-            $mail->addAddress('ourmail@elegy.studio');     //Add a recipient
-            $mail->addAddress('ivan.elegystudio@gmail.com');     //Add a recipient
-            if ($email) {
-                $mail->addReplyTo($email);
-            }
-
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            // $mail->Subject = 'Here is the subject';
-            $mail->Body    = $messageBody;
-            $mail->AltBody = $messageBody;
-
-            $mail->send();
-            echo $mail->Body;
-            echo $mail->AltBody;
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
-    }
+//Recipients
+$mail->Subject = "Новое сообщение из формы";
+$mail->setFrom('mailbot@elegy.studio');
+$mail->addAddress('ourmail@elegy.studio');     //Add a recipient
+if ($email) {
+    $mail->addReplyTo($email);
 }
-Mailer::sendMail($email, $messageBody);
-    echo $messageBody;
+
+//Content
+$mail->isHTML(true);                                  //Set email format to HTML
+// $mail->Subject = 'Here is the subject';
+$mail->Body    = $messageBody;
+// $mail->AltBody = $messageBody;
+
+// echo $mail->Body;
+// echo $mail->AltBody;
+// echo 'Message has been sent';
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+}
+
+echo $messageBody;
+echo $mail->Body;
     // echo $_POST["messenger"]."<br>";
     // echo $_POST["nickname"]."<br>";
     // echo $_POST["email"]."<br>";
