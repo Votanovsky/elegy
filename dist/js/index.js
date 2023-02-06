7423f44e8be4beaf5267e9effad8b692d9a0f6bf
misterbobr <monsieurraspberry@gmail.com>
2023-02-06 21:44:41 +0300

    added ru titles in index.js

--- dist/js/index.js
+++ dist/js/index.js
@@ -244,7 +244,7 @@ async function loadPage() {
     if (window.textInterval) {
         clearInterval(window.textInterval);
     }
-    if (window.innerWidth < mobileWidth && document.title !== 'About Elegy') {
+    if (window.innerWidth < mobileWidth && document.title !== 'About Elegy' && document.title !== 'О Elegy') {
 
         function animateFontColor () {
             const h1FontFamily = gsap.utils.shuffle([...document.querySelectorAll('.char')].filter(()=> 
@@ -473,7 +473,7 @@ async function loadPage() {
     // });
     // Ховер стерлочек для кейсов на главной странице 
 
-    if (document.title === 'About Elegy') {
+    if (document.title === 'About Elegy' || document.title === 'О Elegy') {
         if (!document.querySelector('canvas')) {
             init(mobileWidth);
             tick();
@@ -507,10 +507,10 @@ async function loadPage() {
                 clipPath: "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)"
             })
         }
-        else if (document.title === 'Our cases') {
+        else if (document.title === 'Our cases' || document.title === 'Наши кейсы') {
             loadCases();
         }
-        else if (document.title === 'How we work') {
+        else if (document.title === 'How we work' || document.title === 'Как мы работаем') {
             loadWork();
         }
     }
